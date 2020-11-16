1041080259612f239c9ac83f4fd850077f62c633
masal9pse <returnymgstokh@icloud.com>
2020-11-17 06:06:32 +0900

    Laravel側でCORSの設定

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,22 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import About from '../views/About.vue'
 import Count from '../views/Count.vue'
-
+import Login from '../views/Login'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-        {
-            path: '/',
-            name: 'count',
-            component: Count
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: About
-        },
-    ]
+ mode: 'history',
+ base: process.env.BASE_URL,
+ routes: [
+  {
+   path: '/',
+   name: 'count',
+   component: Count
+  },
+  {
+   path: '/login',
+   name: 'login',
+   component: Login
+  },
+  {
+   path: '/about',
+   name: 'about',
+   component: About
+  },
+ ]
 })                                                                                                                                                                                                                                              
